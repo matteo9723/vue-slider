1ff10efde4b22327bf46284792b9a9a05f48b0a2
matteo9723 <matteolandri7@gmail.com>
2021-11-11 17:49:44 +0100

    esercizio completato

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const app = new Vue ({
-
   el: '#app',
 
   data: {
-    saluto:'ciao'
+  images:['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'],
+  counter : 0
   },
 
   mounted(){},
 
-  methods:{}
+  methods:{
+
+    nextSlide(){
+
+      this.counter++;
+      if(this.counter > this.images.length - 1 ){
+        this.counter=0;
+      }
+      
+    },
+
+    prevSlide(){
+
+      this.counter--;
+      if(this.counter < 0 ){
+        this.counter=this.images.length-1;
+      }
+
+    }
+
+  }
 
 });
+
+  
+
+
+
+// SHORTCUT --> ctrl+F = sostituisci i caratteri uguali velocemente
+  
